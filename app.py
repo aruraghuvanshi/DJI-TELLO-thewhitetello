96932f68787805f74a69603375c118855c89bc96
Aru Raghuvanshi <52784196+hammeroflight@users.noreply.github.com>
2020-09-23 20:28:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import time
 drone = TheWhiteTello()
 
 print('\n\n\t\tPYTHON \033[1;31mUAV\033[0m CONTROLLER')
+
 drone.connect()
 drone.streamon()
 drone.get_battery()          # Get battery level (0 - 100%)
@@ -11,9 +12,7 @@ drone.takeoff()              # take off
 drone.forward(20)            # fly forward 20 cms
 drone.clockwise(30)          # turn left 30 degrees
 drone.up(10)                 # fly up 10 cms
+drone.flip_left()            # cartwheel to the left
 drone.land()                 # land
 
 drone.stop()                 # disconnect drone
-
-
-
