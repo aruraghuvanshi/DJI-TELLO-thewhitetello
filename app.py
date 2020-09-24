4aceecac162fe74e180199fdad49eeaf784ecc4a
Aru Raghuvanshi <52784196+hammeroflight@users.noreply.github.com>
2020-09-24 13:59:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from thewhitetello import TheWhiteTello
-import time, queue
 import threading
-import cv2
 
-q = queue.Queue()
-drone = TheWhiteTello()
-print('\n\n\t\tPYTHON \033[1;31mUAV\033[0m CONTROLLER')
-p1 = threading.Thread(target=drone.recieve)
+drone = TheWhiteTello()             # create instance
+drone.connect()                     # connect 
+print('\n\n\t\tPYTHON UAV CONTROLLER')
+drone.get_battery()                 # check drone power level
+drone.streamon()                    # enable streaming of video
+
+# ------ START VIDEO STREAM IN EXTERNAL WINDOW ------]
+p1 = threading.Thread(target=drone.recieve)  
 p2 = threading.Thread(target=drone.display)
-
-drone.connect()
-drone.get_battery()
-drone.streamon()
-
 p1.start()
 p2.start()
+# ---------------------------------------------------]
 
-
-
+drone.takeoff()
+time.sleep(5)
+drone.forward(20)                   # fly forward 20 cms
+drone.clockwise(90)                 # turn clockwise 90 degrees
+drone.up(20)                        # fly up 20 cms
+drone.land()                        # land
+drone.stop()                        # disconnect
 
 
